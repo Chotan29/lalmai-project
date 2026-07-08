00dbedddccb37c18c133ef0e190fb177af49e6ac
chotan29 <chotan29@github.com>
2026-07-08 16:26:00 +0600

    feat: schedule apply-to-all button + subject pass-mark data fix script [2026-07-08]

--- resources/views/examination/schedule/includes/subject.blade.php
+++ resources/views/examination/schedule/includes/subject.blade.php
@@ -1,4 +1,9 @@
 <div class="form-group">
+    <div class="text-right" style="margin-bottom:5px;">
+        <button type="button" id="applyFirstRowAll" class="btn btn-xs btn-info" title="Copy 1st row's time & marks to every row below">
+            <i class="fa fa-clone"></i> Apply 1st Row Time &amp; Marks to All
+        </button>
+    </div>
     <table id="subjectsTable" class="table table-striped table-bordered table-hover">
         <thead>
         <tr>
@@ -30,3 +35,27 @@
     </table>
 </div>
 @include('includes.scripts.inputMask_script')
+
+<script>
+    jQuery(function ($) {
+        $('#applyFirstRowAll').on('click', function () {
+            var $rows = $('#subject_wrapper tr.option_value');
+            if ($rows.length < 2) {
+                if (window.toastr) toastr.info('Load subjects first (need at least 2 rows).', 'Info:');
+                return;
+            }
+            var fields = ['start_time[]', 'end_time[]', 'full_mark_theory[]',
+                          'pass_mark_theory[]', 'full_mark_practical[]', 'pass_mark_practical[]'];
+            var $first = $rows.first();
+            $rows.slice(1).each(function () {
+                var $row = $(this);
+                fields.forEach(function (f) {
+                    var v = $first.find('input[name="' + f + '"]').val();
+                    var $target = $row.find('input[name="' + f + '"]');
+                    if ($target.length && v !== undefined) $target.val(v);
+                });
+            });
+            if (window.toastr) toastr.success('1st row time & marks applied to all rows. Now set each date.', 'Success:');
+        });
+    });
+</script>
